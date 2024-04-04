244ff8a2b6255faab6be2d12c1420d133aee06b2
AnandRP2030 <anand.rp2030@gmail.com>
2024-04-04 12:35:59 +0530

    instlled formik

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
-import './App.css';
-import Admin_login from './Pages/Admin/admin_login';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Freelancer_login from './Pages/Freelancers/login/freelancer_login';
-import Freelancer_register from './Pages/Freelancers/register/freelancer_register';
-import User_login from './Pages/User/login/user_login';
-import User_register from './Pages/User/register/user_register';
-import Home from './Pages/Common/Home/home';
-import Navbar from './Pages/Common/Navbar/navbar';
-import Footer from './Pages/Common/Footer/footer';
-import Forgot_password from './Pages/User/forgot_password/forgot_password';
+import "./App.css";
+import Admin_login from "./Pages/Admin/admin_login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Freelancer_login from "./Pages/Freelancers/login/freelancer_login";
+import Freelancer_register from "./Pages/Freelancers/register/freelancer_register";
+import User_login from "./Pages/User/login/user_login";
+import User_register from "./Pages/User/register/user_register";
+import Home from "./Pages/Common/Home/home";
+import Navbar from "./Pages/Common/Navbar/navbar";
+import Footer from "./Pages/Common/Footer/footer";
+import Forgot_password from "./Pages/User/forgot_password/forgot_password";
 function App() {
   return (
-    <div className='container-xxl bg-hero m-auto w-100'>
-      <Navbar/>
+    <div className="container-xxl bg-hero m-auto w-100">
+      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          {/* user routes  */}
+          <Route path="/" element={<Home />} />
+          <Route path="/user-login" element={<User_login />} />
+          <Route path="/user-register" element={<User_register />} />
+          <Route path="/user-forgot-password" element={<Forgot_password />} />
+          {/* Freelancer routes  */}
+          <Route path="/freelancer-login" element={<Freelancer_login />} />
+          <Route
+            path="/freelancer-register"
+            element={<Freelancer_register />}
+          />
+          {/* Admin routes  */}
           <Route path="/admin" element={<Admin_login />} />
-          <Route path='/freelancer-login' element={<Freelancer_login/>}/>
-          <Route path='/freelancer-register' element={<Freelancer_register/>}/>
-          <Route path='/user-login' element={<User_login/>}/>
-          <Route path='/user-register' element={<User_register/>}/>
-          <Route path='/user-forgot-password' element={<Forgot_password/>}/>
+          <Route path="/*" element={<h1> Please re-check the Route </h1>} />
         </Routes>
       </BrowserRouter>
-     <Footer/>
+      <Footer />
     </div>
   );
 }
